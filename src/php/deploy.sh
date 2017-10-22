dfdd72b3331f7a4e62d2369bec578fb700263e00
Suguru Shirai <shirai_suguru@cyberagent.co.jp>
2017-10-22 15:42:06 +0900

    deployシェルを修正

--- src/php/deploy.sh
+++ src/php/deploy.sh
@@ -4,3 +4,7 @@
 cd /home/isucon/isubata/repository/isucon2017
 
 git pull origin master
+
+
+cp /home/isucon/isubata/repository/isucon2017/src/php/index.php  /home/isucon/isubata/webapp/php/
+cp /home/isucon/isubata/repository/isucon2017/src/php/deploy.sh  /home/isucon/isubata/webapp/php/
